6ed413e4efe395161fae723534423553ab9453ac
Gerard (Gerry) Caulfield <git@caulfield.me>
2015-11-10 03:14:13 +1100

    Remove unnecessary code from index file now that I've switched to using a readme

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-// random-strings module
-function generateRandomString(){
-  return Math.random().toString(36).replace(/[^a-z]+/g, '');
-}
-
-module.exports = {
-  generate: generateRandomString
-};
-
-
-
-// node-string-algorithms module
-var randomStrings = require('random-strings.js');
-
-// Override parts of another module
-randomStrings.generate = function(){
-	return 'All your base are belong to us';
-}
-
-module.exports = {
-	// String functions go here
-}
-
-
-
-// User's code
 var randomString = require('random-strings');
 console.log(randomString.generate()); // Outputs a random string
 
